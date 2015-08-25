f036325e1c56977b8aa256610c1e8e2e7bc001b6
moloko <mleathes+github@gmail.com>
2015-08-25 17:35:52 +0100

    amend any references to old plugin/repo name; add support for detecting Enterprise Mode in IE11; swap the compat mode and document mode check back around again

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 adapt-IE-Check
 =================
 
-An extension to raise an alert if Internet Explorer is not behaving itself.
+An extension to raise an alert if the wrong version of Internet Explorer is used or it has been configured to run in any mode other than 'Edge Mode'.
 
 An alert will be raised if:
-* The version of IE being used is less than the minimum supported version (defaults to 8)
+* The version of IE being used is less than the minimum supported version (defaults to 8 but can be changed)
+* Enterprise Mode (IE11 only) is enabled
 * Compatibility view is switched on
 * Some form of compatibility mode is active (i.e. the 'document mode' is less than the actual version)
 
 Mainly used in the Adapt Scorm Test but could be used in any course where there's a risk of IE doing one of the above.
+
+In order to change any of the error messages or the minimum supported version of IE you'll have to edit js/adapt-IE-Check.js - if we were to store any of the values in JSON there's good chance Adapt wouldn't even be able to load them in some circumstances.
--- js/adapt-IE-Check.js
+++ js/adapt-IE-Check.js
@@ -1,6 +1,6 @@
 /*
 * adapt-IE-Check
-* License - https://github.com/cgkineo/adapt-documentMode/LICENSE
+* License - https://github.com/cgkineo/adapt-IE-Check/LICENSE
 * Maintainers - Dan Ghost <daniel.ghost@kineo.com>, Matt Leathes <matt.leathes@kineo.com>
 */
  
@@ -23,10 +23,12 @@ define(['./ie-truth'], function(IEtruth) {
 
 		if(ieVersionInfo.TrueVersion < MIN_SUPPORTED_VERSION) {
 			msg = "The version of Internet Explorer you are using is not supported. The lowest supported version is Internet Explorer v" + MIN_SUPPORTED_VERSION;
+		} else if (ieVersionInfo.TrueVersion < 9 && ('spellcheck' in document.createElement('textarea'))) {
+			msg = "Your browser reports that Enterprise Mode is enabled; this is not supported."; 
 		} else if (wrongDocMode) {
 			msg = "Your browser reports that it is running in a document mode (" + document.documentMode + ") that is lower than the version of your browser (" + ieVersionInfo.TrueVersion + "). This will disable some critical functionality in your course. Please contact Kineo for support.";
 		} else if(ieVersionInfo.CompatibilityMode) {
-			msg = "Your browser reports that Compatibility View is enabled, which is not supported. Please check your Compatibility View settings (Tools > Compatibility View settings) to ensure the site this course is running from is not listed.";
+			msg = "Your browser reports that Compatibility View is enabled; this is not supported. Please check your Compatibility View settings (Tools > Compatibility View settings) to ensure the site this course is running from is not listed.";
 		}
 
 		if(msg !== undefined) alert(msg);
