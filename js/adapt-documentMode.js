2e38367f74c6d573f0c42b96a15ab4ae7b10b10a
moloko <mleathes+github@gmail.com>
2015-08-25 16:04:32 +0100

    swap the doc mode and compat mode checks around so that IE11 doesn't report compat view as being on when it's actually that the document mode is wrong

--- js/adapt-documentMode.js
+++ js/adapt-documentMode.js
@@ -23,10 +23,10 @@ define(['./ie-truth'], function(IEtruth) {
 
 		if(ieVersionInfo.TrueVersion < MIN_SUPPORTED_VERSION) {
 			msg = "The version of Internet Explorer you are using is not supported. The lowest supported version is Internet Explorer v" + MIN_SUPPORTED_VERSION;
-		} else if(ieVersionInfo.CompatibilityMode) {
-			msg = "Your browser reports that Compatibility View is enabled, which is not supported. Please check your Compatibility View settings (Tools > Compatibility View settings) to ensure the site this course is running from is not listed.";
 		} else if (wrongDocMode) {
 			msg = "Your browser reports that it is running in a document mode (" + document.documentMode + ") that is lower than the version of your browser (" + ieVersionInfo.TrueVersion + "). This will disable some critical functionality in your course. Please contact Kineo for support.";
+		} else if(ieVersionInfo.CompatibilityMode) {
+			msg = "Your browser reports that Compatibility View is enabled, which is not supported. Please check your Compatibility View settings (Tools > Compatibility View settings) to ensure the site this course is running from is not listed.";
 		}
 
 		if(msg !== undefined) alert(msg);
